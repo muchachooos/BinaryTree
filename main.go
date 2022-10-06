c2684c3522ff3b353689c40b675fa49c6754c829
muchachooos <odnorogg@bk.ru>
2022-10-06 18:04:28 +0300

    Update main.go

--- main.go
+++ main.go
@@ -4,38 +4,49 @@ import "fmt"
 
 func main() {
 
-	slice := []int{1, 2, 34, 7, 27, 65, 77, 22, 46, 3}
-
-	var x int
-
-	for i := range slice {
-		x += slice[i]
-	}
-	average := x / len(slice)
-
-	difs := make([]int, len(slice))
-	var minNum int
-
-	for i := range slice {
-		dif := slice[i] - average
-		difs[i] = dif
-		if dif < 0 {
-			difs[i] = dif * (-1)
-		}
-		if difs[minNum] >= difs[i] {
-			minNum = i
-		}
-	}
-
+	//slice := []int{1, 2, 34, 7, 27, 65, 77, 22, 46, 3}
+	//
+	//var x int
+	//
+	//for i := range slice {
+	//	x += slice[i]
+	//}
+	//average := x / len(slice)
+	//
+	//difs := make([]int, len(slice))
+	//var minNum int
+	//
+	//for i := range slice {
+	//	dif := slice[i] - average
+	//	difs[i] = dif
+	//	if dif < 0 {
+	//		difs[i] = dif * (-1)
+	//	}
+	//	if difs[minNum] >= difs[i] {
+	//		minNum = i
+	//	}
+	//}
 	//fmt.Println(minNum)
 	//fmt.Println(slice[minNum])
 
 	el1 := node{
-		21,
+		9,
 		nil,
 		nil,
 	}
-	addToTree(&el1, 33)
+
+	testSlice := []int{3, 4, 5, 8, 19}
+
+	for i := range testSlice {
+		addToTree(&el1, testSlice[i])
+	}
+
+	fmt.Println("9", el1.val)
+	fmt.Println("3", el1.left.val)
+	fmt.Println("4", el1.left.right.val)
+	fmt.Println("5", el1.left.right.right.val)
+	fmt.Println("8", el1.left.right.right.right.val)
+	fmt.Println("19", el1.right.val)
 }
 
 type node struct {
@@ -44,33 +55,37 @@ type node struct {
 	right *node
 }
 
-func addToTree(root *node, addEl int) {
+func addToTree(root *node, addVal int) {
 	if root == nil {
 		fmt.Println("Ck!")
 		return
 	}
 
-	if addEl < root.val {
+	if addVal < root.val {
 		if root.left == nil {
-			root.left = &node{addEl, nil, nil}
+			root.left = &node{addVal, nil, nil}
 		} else {
-			if addEl < root.left.val {
-				root.left.left = &node{addEl, nil, nil}
-			} else {
-				root.left.right = &node{addEl, nil, nil}
-			}
+			addToTree(root.left, addVal)
 		}
 	} else {
 		if root.right == nil {
-			root.right = &node{addEl, nil, nil}
+			root.right = &node{addVal, nil, nil}
 		} else {
-			if addEl > root.right.val {
-				root.right.right = &node{addEl, nil, nil}
-			} else {
-				root.right.left = &node{addEl, nil, nil}
-			}
+			addToTree(root.right, addVal)
 		}
 	}
 }
 
 //root.left.left.val = addEl
+
+//if addVal < root.left.val {
+//root.left.left = &node{addVal, nil, nil}
+//} else {
+//root.left.right = &node{addVal, nil, nil}
+//}
+//--------------------------------------
+//if addVal > root.right.val {
+//root.right.right = &node{addVal, nil, nil}
+//} else {
+//root.right.left = &node{addVal, nil, nil}
+//}
