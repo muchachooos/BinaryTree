4db46f8e8fe4b91b3e1ffe2ca66595bc88172973
muchachooos <odnorogg@bk.ru>
2022-10-07 15:37:27 +0300

    Create main_test.go

---
+++ main_test.go
@@ -0,0 +1,41 @@
+package main
+
+import "testing"
+
+func Test_addToEbala(t *testing.T) {
+	el1 := node{
+		9,
+		nil,
+		nil,
+	}
+
+	testSlice := []int{3, 4, 5, 8, 19}
+
+	for i := range testSlice {
+		addToTree(&el1, testSlice[i])
+	}
+
+	if el1.val != 9 {
+		t.Fail()
+	}
+
+	if el1.left.val != 3 {
+		t.Fail()
+	}
+
+	if el1.left.right.val != 4 {
+		t.Fail()
+	}
+
+	if el1.left.right.right.val != 5 {
+		t.Fail()
+	}
+
+	if el1.left.right.right.right.val != 8 {
+		t.Fail()
+	}
+
+	if el1.right.val != 19 {
+		t.Fail()
+	}
+}
