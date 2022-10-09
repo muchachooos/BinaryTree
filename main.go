27105fb961a6c210c88fd0151ef6feba1b8f347a
muchachooos <odnorogg@bk.ru>
2022-10-09 16:08:44 +0300

    Update main.go

--- main.go
+++ main.go
@@ -4,32 +4,35 @@ import "fmt"
 
 func main() {
 
-	slice := []int{1, 2, 34, 7, 27, 65, 77, 22, 46, 3}
+	//slice := []int{1, 2, 34, 7, 27, 65, 77, 22, 46, 3}
 
-	var x int
+	//var x int
+	//
+	//for i := range slice {
+	//	x += slice[i]
+	//}
+	//average := x / len(slice)
+	//
+	//difs := make([]int, len(slice))
+	//var minNum int
+	//
+	//for i := range slice {
+	//	dif := slice[i] - average
+	//	difs[i] = dif
+	//	if dif < 0 {
+	//		difs[i] = dif * (-1)
+	//	}
+	//	if difs[minNum] >= difs[i] {
+	//		minNum = i
+	//	}
+	//}
+	//
+	//fmt.Println(minNum)
+	//fmt.Println(slice[minNum])
 
-	for i := range slice {
-		x += slice[i]
-	}
-	average := x / len(slice)
+	testSlice := []int{3, 4, 5, 8, 19}
 
-	difs := make([]int, len(slice))
-	var minNum int
-
-	for i := range slice {
-		dif := slice[i] - average
-		difs[i] = dif
-		if dif < 0 {
-			difs[i] = dif * (-1)
-		}
-		if difs[minNum] >= difs[i] {
-			minNum = i
-		}
-	}
-
-	fmt.Println(minNum)
-	fmt.Println(slice[minNum])
-
+	deleteElement()
 }
 
 type node struct {
@@ -38,9 +41,29 @@ type node struct {
 	right *node
 }
 
+func deleteElement(root *node, delVal int) {
+	if root != nil {
+		fmt.Println("Ck")
+		return
+	}
+
+	if root.val != delVal {
+		if delVal < root.val {
+			if root.left.val != delVal {
+				if delVal < root.left.val {
+					deleteElement(root.left, delVal)
+				} else {
+					root = nil
+				}
+			}
+		}
+	}
+
+}
+
 func addToTree(root *node, addVal int) {
 	if root == nil {
-		fmt.Println("Ck!")
+		fmt.Println("Ck")
 		return
 	}
 
