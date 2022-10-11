ea99e9a6f2bdf8aaea8539e7e805091058f87465
muchachooos <odnorogg@bk.ru>
2022-10-11 18:33:05 +0400

    Update main_test.go

--- main_test.go
+++ main_test.go
@@ -6,7 +6,7 @@ import (
 
 func Test_FunctionTree(t *testing.T) {
 
-	el1 := node{
+	rootElement := node{
 		9,
 		nil,
 		nil,
@@ -16,55 +16,59 @@ func Test_FunctionTree(t *testing.T) {
 
 	//Add function test
 	for i := range testSlice {
-		addElement(&el1, testSlice[i])
+		addElement(&rootElement, testSlice[i])
 	}
 
-	if el1.val != 9 {
+	if rootElement.val != 9 {
 		t.Log("add_1 failed")
 		t.Fail()
 	}
 
-	if el1.left.val != 3 {
+	if rootElement.left.val != 3 {
 		t.Log("add_2 failed")
 		t.Fail()
 	}
 
-	if el1.left.right.val != 4 {
+	if rootElement.left.right.val != 4 {
 		t.Log("add_3 failed")
 		t.Fail()
 	}
 
-	if el1.left.right.right.val != 5 {
+	if rootElement.left.right.right.val != 5 {
 		t.Log("add_4 failed")
 		t.Fail()
 	}
 
-	if el1.left.right.right.right.val != 8 {
+	if rootElement.left.right.right.right.val != 8 {
 		t.Log("add_5 failed")
 		t.Fail()
 	}
 
-	if el1.right.val != 19 {
+	if rootElement.right.val != 19 {
 		t.Log("add_6 failed")
 		t.Fail()
 	}
 
 	// Search function test
 	for j := range testSlice {
-		res := searchElement(&el1, testSlice[j])
+		res := searchElement(&rootElement, testSlice[j])
 		if res == false {
 			t.Log("Search failed")
 			t.Fail()
 		}
 	}
 
-	//different value test
-	res := searchElement(&el1, 344)
+	//Different value test
+	res := searchElement(&rootElement, 344)
 	if res == true {
 		t.Log("Search failed")
 		t.Fail()
 	}
 
 	//Delete function test
-
+	deleteElement(&rootElement, 4)
+	if rootElement.left.right != nil {
+		t.Log("Delete failed")
+		t.Fail()
+	}
 }
